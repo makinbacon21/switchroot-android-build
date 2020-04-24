7d0d188d345aa7a97200145534e7292c1e15735e
Pablo Zaidenvoren <zaiden@gmail.com>
2020-04-23 20:12:00 -0400

    derp

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ cd /root/android/lineage
 source build/envsetup.sh
 export USE_CCACHE=1
 ccache -M 50G
-#lunch lineage_$ROM_NAME-userdebug
+lunch lineage_$ROM_NAME-userdebug
 
 sed -i '/BOARD_KERNEL_IMAGE_NAME := zImage/a BOARD_MKBOOTIMG_ARGS    += --cmdline " "' ~/android/lineage/device/nvidia/foster/BoardConfig.mk
 
-#make bacon
+make bacon
