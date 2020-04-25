ed524fc4aac89999b00476fc37aa8b593faf42ed
Pablo Zaidenvoren <zaiden@gmail.com>
2020-04-25 10:36:23 -0400

    forgot mount

--- extract-images.sh
+++ extract-images.sh
@@ -11,7 +11,7 @@ fi
 
 mkdir -p ~/Downloads/images
 
-CONTAINER=$(docker run -d -i switchroot:build-android)
+CONTAINER=$(docker run --rm -ti -e ROM_NAME=$ROM_NAME -v "$PWD"/android:/root/android switchroot:build-android)
 docker exec $CONTAINER bash -c "mkdir /extract"
 docker exec $CONTAINER bash -c "cp /root/android/lineage/out/target/product/$ROM_NAME/lineage-16.0-*-UNOFFICIAL-$ROM_NAME.zip /extract" 
 docker exec $CONTAINER bash -c "cp /root/android/lineage/out/target/product/$ROM_NAME/obj/KERNEL_OBJ/arch/arm64/boot/dts/tegra210-icosa.dtb /extract"
