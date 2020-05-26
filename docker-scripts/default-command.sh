cab9cb589734a25522c285223857844a450c283f
classic-gentleman <43123392+classic-gentleman@users.noreply.github.com>
2020-05-26 17:33:04 -0300

    Allow for tool-driven workflows
    By using a var to control whether to update or build we can have development workflows such as sync - patch - build driven by a local tool such a script.

--- docker-scripts/default-command.sh
+++ docker-scripts/default-command.sh
@@ -4,9 +4,15 @@ if [ "$(ls -A ./android/lineage)" ]; then
     echo "Sources found. Skipping..."
 else
     echo "Getting sources..."
+    ACTION="default"
     ./get-sources.sh
 fi
 
-./reset-changes-update-sources.sh
-./repopic-and-patch.sh
-./build.sh
+if [ "$ACTION" != "noupdate" ]; then
+    ./reset-changes-update-sources.sh
+    ./repopic-and-patch.sh
+fi
+
+if [ "$ACTION" != "nobuild" ]; then
+    ./build.sh
+fi
