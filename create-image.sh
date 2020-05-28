57167487ba25023a517f3bba63d8788606df0b28
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2020-05-28 10:47:51 -0400

    Fetch remote image to use cache when building

--- create-image.sh
+++ create-image.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Fetch latest image from the registry to take advantage of the cache
+docker pull pablozaiden/switchroot-android-build:latest
+
+# Build locally in case of changes
 docker build -t pablozaiden/switchroot-android-build ./docker-scripts
