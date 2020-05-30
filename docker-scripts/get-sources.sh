b8d215271070c9a20333e1bfc0094d4692d4ee9d
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2020-05-30 11:00:56 -0400

    remove depth 1

--- docker-scripts/get-sources.sh
+++ docker-scripts/get-sources.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /root/android/lineage
-repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-16.0
-repo sync -c -j16
+repo init -u https://github.com/LineageOS/android.git -b lineage-16.0
+repo sync -j16
 
 cd /root/android/lineage/.repo
-git clone --depth 1 https://gitlab.com/switchroot/android/manifest.git local_manifests
-repo sync -c -j16
+git clone https://gitlab.com/switchroot/android/manifest.git local_manifests
+repo sync -j16
--- docker-scripts/reset-changes-update-sources.sh
+++ docker-scripts/reset-changes-update-sources.sh
@@ -8,4 +8,4 @@ git pull
 
 cd ../..
 
-repo sync -c -j16
+repo sync -j16
