d7ce62753458025f780596d99264a28e68901f7d
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2020-06-05 10:13:55 -0400

    when building base image, avoid cache

--- create-base-image.sh
+++ create-base-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build locally in case of changes
-docker build -t pablozaiden/switchroot-android-build:base-latest -f ./docker-scripts/Dockerfile-base ./docker-scripts
+docker build --no-cache -t pablozaiden/switchroot-android-build:base-latest -f ./docker-scripts/Dockerfile-base ./docker-scripts
