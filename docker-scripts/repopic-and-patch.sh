b0783a2440cf4f89910b4924506905223323a5de
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2020-08-21 10:04:39 -0400

    removed joycon-p and backuptool-cleanup-p repopics

--- docker-scripts/repopic-and-patch.sh
+++ docker-scripts/repopic-and-patch.sh
@@ -5,10 +5,8 @@ ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-en
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-shieldtech-p
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-beyonder-p
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-nvgpu-p
-${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t joycon-p
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t icosa-bt
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py 272671
-${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t backuptool-cleanup-p
 
 cd ${BUILDBASE}/android/lineage/frameworks/base
 patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/frameworks_base-rsmouse.patch
