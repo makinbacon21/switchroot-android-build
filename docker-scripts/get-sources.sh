24a96893138d5b8d03651690886e5a2b57392d59
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2020-09-30 16:08:12 -0400

    hardcoded manifest

--- docker-scripts/get-sources.sh
+++ docker-scripts/get-sources.sh
@@ -7,5 +7,9 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-17.1
 repo sync -j${JOBS}
 
 cd ${BUILDBASE}/android/lineage/.repo
-git clone https://gitlab.com/switchroot/android/manifest.git -b lineage-17.1 local_manifests
+# UNCOMMENT THIS WHEN THE 17.1 BRANCH BECOMES AVAILABLE AND DELETE THE HARDCODED MANIFEST
+# git clone https://gitlab.com/switchroot/android/manifest.git -b lineage-17.1 local_manifests
+mkdir local_manifests
+cp ${BUILDBASE}/switch.xml ./local_manifests/switch.xml
+
 repo sync -j${JOBS}
