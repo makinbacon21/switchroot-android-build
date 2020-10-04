7b0901f7afcc141ab8eec073e85502899e23a4a4
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2020-10-04 15:13:12 -0400

    Patch for current coreboot

--- docker-scripts/repopic-and-patch.sh
+++ docker-scripts/repopic-and-patch.sh
@@ -8,6 +8,12 @@ ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t icosa-bt-
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py 287339
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py 284553
 
+# Temporary patch to use current coreboot
+wget -O .repo/android_device_nvidia_foster.patch https://gitlab.com/ZachyCatGames/q-tips-guide/-/raw/master/res/android_device_nvidia_foster.patch
+cd ${BUILDBASE}/android/lineage/device/nvidia/foster
+patch -p1 < ${BUILDBASE}/android/lineage/.repo/android_device_nvidia_foster.patch
+rm ${BUILDBASE}/android/lineage/.repo/android_device_nvidia_foster.patch
+
 cd ${BUILDBASE}/android/lineage/bionic
 patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/bionic_intrinsics.patch
 
