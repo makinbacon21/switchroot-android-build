4fbaaed4d0b8d5f17e64452c31e0d3fd07a8b776
Thomas Makin <halorocker89@gmail.com>
2020-12-17 10:44:14 -0500

    Update repopic-and-patch.sh

--- build-scripts/repopic-and-patch.sh
+++ build-scripts/repopic-and-patch.sh
@@ -11,3 +11,11 @@ ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py 284553
 
 cd ${BUILDBASE}/android/lineage/bionic
 patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/bionic_intrinsics.patch
+cd ${BUILDBASE}/android/lineage/frameworks/base
+patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/frameworks_base_nvcpl.patch
+cd ${BUILDBASE}/android/lineage/kernel/nvidia/linux-4.9/kernel/kernel-4.9
+patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/oc-android10.patch
+cd ${BUILDBASE}/android/lineage/hardware/nvidia/platform/t210/icosa
+patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/wdt.patch
+cd ${BUILDBASE}/android/lineage/hardware/nintendo/joycond
+patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/joycond10.patch
