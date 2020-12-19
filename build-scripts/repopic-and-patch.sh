0921ac535f53128525fbf4799e0a98e7269c2d0d
Thomas Makin <halorocker89@gmail.com>
2020-12-19 17:32:37 -0500

    Trash the joycond patch to possibly add optionally

--- build-scripts/repopic-and-patch.sh
+++ build-scripts/repopic-and-patch.sh
@@ -13,5 +13,3 @@ cd ${BUILDBASE}/android/lineage/bionic
 patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/bionic_intrinsics.patch
 cd ${BUILDBASE}/android/lineage/frameworks/base
 patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/frameworks_base_nvcpl.patch
-cd ${BUILDBASE}/android/lineage/hardware/nintendo/joycond
-patch -p1 < ${BUILDBASE}/android/lineage/.repo/local_manifests/patches/joycond10.patch
