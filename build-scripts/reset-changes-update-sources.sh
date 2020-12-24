2aa225aa4029e8933b9b38a8fea76655be7626cf
Thomas Makin <halorocker89@gmail.com>
2020-12-24 18:11:20 -0500

    add git clean command to fix unclean builds

--- build-scripts/reset-changes-update-sources.sh
+++ build-scripts/reset-changes-update-sources.sh
@@ -6,6 +6,7 @@ cd ${BUILDBASE}/android/lineage/bootable/
 rm -rf ./recovery/ # Delete it, just in case it was repleaced by twrp
 
 cd ${BUILDBASE}/android/lineage
+repo forall -c 'git clean -fdd'
 repo forall -c 'git reset --hard'
 
 cd .repo/local_manifests
