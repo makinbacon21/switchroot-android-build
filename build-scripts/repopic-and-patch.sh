c847fdfca930d2723bcf4a26bb0b58a822b1e350
Pablo Zaidenvoren <pabloz@lagash.com>
2021-01-01 08:25:47 -0500

    new repopics and patches

--- build-scripts/repopic-and-patch.sh
+++ build-scripts/repopic-and-patch.sh
@@ -2,8 +2,8 @@
 
 cd ${BUILDBASE}/android/lineage/
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-enhancements-q
-${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-nvgpu-q
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t icosa-bt-lineage-17.1
+${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-shieldtech-q
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py -t nvidia-beyonder-q
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py 287339
 ${BUILDBASE}/android/lineage/vendor/lineage/build/tools/repopick.py 284553
